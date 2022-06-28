a242eb6d51c9d2f2e547da3740d418861f158c87
Helton Rios <helton.placido@gmail.com>
2022-06-28 14:40:51 -0300

    primeiro push

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Helton Rios"
+            content="lorem ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor"
+          />
+          <Post
+            author="Carolina Rios"
+            content="lorem ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
